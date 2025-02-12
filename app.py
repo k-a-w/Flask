7a63a8fa7e3087edb1450f8f12ae14136f7c5536
k-a-w <144478303+k-a-w@users.noreply.github.com>
2025-02-12 15:19:15 -0600

    Adding images and updating templates

--- app.py
+++ app.py
@@ -2,12 +2,21 @@
 #Course: MATH 2213 - Fundamentals of Data Science
 #Semester: Spring 2025
 
-from flask import Flask, render_template #this imports the flask library
+from flask import Flask, render_template, request #this imports the flask library
 app = Flask(__name__) #initiates a private flask object
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def madlib():
+    return render_template("madlib.html")
+
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("result.html", result = result)
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000) #in video port set to 5000
